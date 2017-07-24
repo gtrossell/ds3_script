b4e759bc1a58f408127fb52e7c8fb91fb6f9faa0
Sage Thomas <sage.thomas@outlook.com>
2017-07-24 12:35:34 -0600

    create client and get bucket works

--- tool.groovy
+++ tool.groovy
@@ -3,11 +3,13 @@ package spectra
 @GrabResolver(name='Spectra-Github', root='http://dl.bintray.com/spectralogic/ds3/')
 @Grapes([
   @Grab(group='com.spectralogic.ds3', module='ds3-sdk', version='3.4.0'),
-  @Grab(group='org.slf4j', module='slf4j-simple', version='1.7.21')
+  @Grab(group='org.slf4j', module='slf4j-simple', version='1.7.21'),
+  @GrabConfig(systemClassLoader = true)
 ])
 
 import org.codehaus.groovy.runtime.InvokerHelper
 import org.codehaus.groovy.control.CompilerConfiguration
+import org.codehaus.groovy.control.customizers.ImportCustomizer
 
 import java.io.IOException
 
@@ -20,24 +22,15 @@ import static spectra.ClientCommands.*
 
 class Tool extends Script {
   def run() {
-    def binding = new Binding()
-    def config = new CompilerConfiguration()
-    config.scriptBaseClass = 'SpectraDSL'
-    def shell = new GroovyShell(this.class.classLoader, binding, config)
-    
-    // Set enums as shell variables
-    binding.setVariable('info', info)
+    def shell = new GroovyShell(this.class.classLoader, buildBinding(), buildConfig())
 
     try {
       def console = new ConsoleReader()
-      // console.addCompleter(new FileNameCompleter())
       console.setPrompt(Globals.PROMPT)
       println Globals.init_message(console.getTerminal().getWidth())
       def line
-      // shell.run("show the client info")
       while (line = console.readLine()) {
-        // if (line.split(' ').length % 2 == 1) line = line.trim() + '()'
-        println shell.evaluate(line).call()
+        println shell.evaluate(line)
       }
     } catch (IOException e) {
       e.printStackTrace()
@@ -53,4 +46,28 @@ class Tool extends Script {
   static void main(String[] args) {
     InvokerHelper.runScript(Tool, args)
   }
+
+  /** Builds object to pass imports into the shell */
+  private buildImportCustomizer() {
+    def importCustomizer = new ImportCustomizer()
+    importCustomizer.addImport('com.spectralogic.ds3client.models.common.Credentials')
+    importCustomizer.addImport('com.spectralogic.ds3client.Ds3ClientBuilder')
+    importCustomizer.addStarImports('com.spectralogic.ds3client.commands')
+    
+    return importCustomizer
+  }
+
+  /** Builds shell configuration */
+  private buildConfig() {
+    def config = new CompilerConfiguration()
+    config.addCompilationCustomizers(buildImportCustomizer())
+    config.scriptBaseClass = 'SpectraDSL'
+    return config
+  }
+
+  /** Builds shell binding */
+  private buildBinding() {
+    new Binding()
+  }
+
 }
