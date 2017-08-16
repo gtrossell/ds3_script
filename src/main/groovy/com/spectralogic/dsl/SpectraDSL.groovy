82140207b34e7c9e51ff7c6a7b8f1c642e889ee5
Sage Thomas <sage.thomas@outlook.com>
2017-08-16 11:37:00 -0600

    corrected comments

--- src/main/groovy/com/spectralogic/dsl/SpectraDSL.groovy
+++ src/main/groovy/com/spectralogic/dsl/SpectraDSL.groovy
@@ -4,17 +4,10 @@ import com.spectralogic.dsl.models.BpClientFactory
 import java.nio.file.Path
 import java.nio.file.Paths
 
-/**
- * This is the customized SpectraShell that contains the DSL and acts similar
- * to GroovyShell
- */
+/** functions available to the user from the shell */
 abstract class SpectraDSL extends Script {
 
-  /**
-   * Points to a Global function since functions in an abstract class cannot be
-   * be directly referenced and this function is used before init 
-   * @return BpClient with given attributes or environment variables
-   */
+  /** @return BpClient with given attributes or environment variables */
   def createBpClient(String endpoint="", String accessId="", 
                       String secretKey="", Boolean https=false) {
     return new BpClientFactory().createBpClient(endpoint, accessId, secretKey, https, environment)
