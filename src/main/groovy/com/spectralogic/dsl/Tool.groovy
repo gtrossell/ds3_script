b78cf716425f17567366bb5bc3edac2589c53f4f
Sage Thomas <sage.thomas@outlook.com>
2017-09-05 17:05:00 -0600

    Created completer for DSL functions

--- src/main/groovy/com/spectralogic/dsl/Tool.groovy
+++ src/main/groovy/com/spectralogic/dsl/Tool.groovy
@@ -3,6 +3,7 @@ package com.spectralogic.dsl
 import com.spectralogic.ds3client.networking.FailedRequestException
 import com.spectralogic.dsl.commands.ShellCommandFactory
 import com.spectralogic.dsl.exceptions.BpException
+import com.spectralogic.dsl.helpers.DslCompleter
 import com.spectralogic.dsl.helpers.Globals
 import com.spectralogic.dsl.helpers.LogRecorder
 import com.spectralogic.ds3client.utils.Guard
@@ -10,7 +11,6 @@ import jline.console.ConsoleReader
 import jline.console.UserInterruptException
 import org.apache.http.conn.ConnectTimeoutException
 import org.codehaus.groovy.runtime.InvokerHelper
-import org.codehaus.groovy.tools.shell.util.SimpleCompletor
 import org.slf4j.LoggerFactory
 
 
@@ -36,6 +36,7 @@ class Tool extends Script {
     def console = new ConsoleReader()
     console.setPrompt(Globals.PROMPT)
     console.setHandleUserInterrupt(true)
+    console.addCompleter(new DslCompleter(shell))
     println Globals.initMessage(console.getTerminal().getWidth())
 
     try {
@@ -48,7 +49,8 @@ class Tool extends Script {
 
       while (true) {
         try {
-          setAutoComplete(console, shell)
+//          setAutoComplete(console, shell)
+
           def line = console.readLine()
           def result = evaluate(shell, line, commandFactory)
           println Globals.RETURN_PROMPT + result
@@ -66,18 +68,20 @@ class Tool extends Script {
     }
   }
 
-  private static setAutoComplete(ConsoleReader console, GroovyShell shell) {
-    def phrases = []
-    shell.getContext().variables.each { name, obj ->
-      phrases.add(name)
-      phrases.addAll(obj.class.getDeclaredMethods().findAll { !it.isSynthetic() }.collect { "${name}.${it.name}()".toString() })
-    }
-    console.addCompleter(new SimpleCompletor(phrases.toArray(new String[phrases.size()])))
-  }
+//  private static setAutoComplete(ConsoleReader console, GroovyShell shell) {
+//    def phrases = []
+//    shell.getContext().variables.each { name, obj ->
+//      phrases.add(name)
+//      phrases.addAll(obj.class.getDeclaredMethods().findAll { !it.isSynthetic() }.collect { "${name}.${it.name}()".toString() })
+//    }
+//    console.addCompleter(new SimpleCompletor(phrases.toArray(new String[phrases.size()])))
+//
+//    console.addCompleter(new FileNameCompleter()) // TODO: make sure '\' is accepted for file locations
+//  }
 
   /** Logic for parsing and evaluating a line */
   private static String evaluate(shell, line, commandFactory) {
-    if (new Guard().isStringNullOrEmpty(line)) return ''
+    if (Guard.isStringNullOrEmpty(line)) return ''
     
     if (line.startsWith(':')) {
       /* command */
