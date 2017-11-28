cf58d8a760e8e2a9ebab22f0a3b6407c45c1f544
Sage Thomas <sage.thomas@outlook.com>
2017-11-27 23:15:30 -0700

    updated tests

--- src/test/groovy/com/spectralogic/dsl/test/models/BpBucketTest.groovy
+++ src/test/groovy/com/spectralogic/dsl/test/models/BpBucketTest.groovy
@@ -1,5 +1,6 @@
 import com.spectralogic.dsl.exceptions.BpException
 import com.spectralogic.dsl.models.BpClientBuilder
+import org.apache.commons.io.FileUtils
 import org.junit.Test
 
 import java.nio.file.Files
@@ -20,8 +21,9 @@ class BpBucketTest extends GroovyTestCase {
     bucket.putBulk(["${homePath}/test-data/dir1/txt1.txt",
                     "${homePath}/test-data/dir1/txt2.txt",
                     "${homePath}/test-data/dir1/txt3.txt"])
+    bucket.putBulk("$homePath/test-data/dir3/txt6.txt", "remote/")
 
-    assertEquals 5, bucket.objects().size()
+    assertEquals 6, bucket.objects().size()
     assertEquals bucket.objects().size(), bucket.reload().objects().size()
     assertEquals 'txt1.txt', bucket.object('txt1.txt').name
     assertEquals 2, bucket.objects('txt2.txt', 'txt3.txt').size()
@@ -33,10 +35,19 @@ class BpBucketTest extends GroovyTestCase {
     objNames.each {
       def file = Paths.get("${homePath}/test-data/tmp/$it")
       assert Files.exists(file)
-      Files.delete(file)
     }
 
+    bucket.getBulk("remote/txt6.txt", "${homePath}/test-data/tmp/")
+    def file = new File("${homePath}/test-data/tmp/remote/txt6.txt")
+    assert file.exists()
+
+    FileUtils.cleanDirectory(new File("${homePath}/test-data/tmp/"))
+
     shouldFail BpException.class, { bucket.delete() }
+
+    bucket.deleteObjects([bucket.object('txt1.txt')])
+    assertEquals 5, bucket.objects().size()
+
     bucket.deleteAllObjects()
     assertEquals 0, bucket.objects().size()
     bucket.delete()
--- src/test/groovy/com/spectralogic/dsl/test/models/BpClientTest.groovy
+++ src/test/groovy/com/spectralogic/dsl/test/models/BpClientTest.groovy
@@ -1,3 +1,5 @@
+package com.spectralogic.dsl.test.models
+
 import com.spectralogic.dsl.models.BpClientBuilder
 import org.junit.Test
 
@@ -7,8 +9,13 @@ class BpClientTest extends GroovyTestCase {
   @Test
   void testClient() throws IOException {
     def client = new BpClientBuilder().create()
+
+    def bucketCount = client.buckets().size()
+
     def bucketName = 'test_bucket_' + (new Random().nextInt(10 ** 4))
     def bucket = client.createBucket(bucketName)
+
+    assertEquals bucketCount + 1, client.buckets().size()
     assertEquals bucketName, bucket.name
     assertEquals bucket.name, client.bucket(bucketName).name
     bucket.delete()
