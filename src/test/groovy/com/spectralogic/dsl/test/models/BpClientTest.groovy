7101bac3e002e7de87d8e6215b8f878faf955748
Sage Thomas <sage.thomas@outlook.com>
2018-02-13 23:24:02 -0700

    updated client test

--- src/test/groovy/com/spectralogic/dsl/test/models/BpClientTest.groovy
+++ src/test/groovy/com/spectralogic/dsl/test/models/BpClientTest.groovy
@@ -6,20 +6,20 @@ import org.junit.Test
 /** Tests BpClient */
 class BpClientTest extends GroovyTestCase {
 
-  @Test
-  void testClient() throws IOException {
-    def client = new BpClientBuilder().create()
+    @Test
+    void testClient() throws IOException {
+        def client = new BpClientBuilder().create()
 
-    def bucketCount = client.buckets().size()
+        def bucketCount = client.buckets().size()
 
-    def bucketName = 'test_bucket_' + (new Random().nextInt(10 ** 4))
-    def bucket = client.createBucket(bucketName)
+        def bucketName = 'test_bucket_' + (new Random().nextInt(10**4))
+        def bucket = client.createBucket(bucketName)
 
-    assertEquals bucketCount + 1, client.buckets().size()
-    assertEquals bucketName, bucket.name
-    assertEquals bucket.name, client.bucket(bucketName).name
-    bucket.delete()
-    shouldFail { client.bucket(bucketName) }
-  }
+        assertEquals bucketCount + 1, client.buckets().size()
+        assertEquals bucketName, bucket.name
+        assertEquals bucket.name, client.bucket(bucketName).name
+        bucket.delete()
+        shouldFail { client.bucket(bucketName) }
+    }
 
 }
