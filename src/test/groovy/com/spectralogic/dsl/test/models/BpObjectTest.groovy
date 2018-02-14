1af25b078485843d61b8c2b9be2f795703edb45d
Sage Thomas <sage.thomas@outlook.com>
2018-02-13 23:30:10 -0700

    updated object test

--- src/test/groovy/com/spectralogic/dsl/test/models/BpObjectTest.groovy
+++ src/test/groovy/com/spectralogic/dsl/test/models/BpObjectTest.groovy
@@ -1,3 +1,5 @@
+package com.spectralogic.dsl.test.models
+
 import com.spectralogic.dsl.models.BpClientBuilder
 import org.junit.Test
 
@@ -7,34 +9,34 @@ import java.nio.file.Paths
 /** Tests BpObject */
 class BpObjectTest extends GroovyTestCase {
 
-  @Test
-  void testObject() throws IOException {
-    def client = new BpClientBuilder().create()
-    def bucketName = 'test_bucket_' + (new Random().nextInt(10 ** 4))
-    def bucket = client.createBucket(bucketName)
-    
-    def homePath = new File("").getAbsoluteFile().toString()
-    def fileName = 'txt1.txt'
-    bucket.putBulk("${homePath}/test-data/dir1/" + fileName)
-    def object = bucket.object(fileName)
-    def meta = object.getMetadata()
-    assert meta['name']
-    assert meta['size'] == object.size
-    assert 10 < object.size
-    assert meta['owner']
-    assert meta['bucketName']
+    @Test
+    void testObject() throws IOException {
+        def client = new BpClientBuilder().create()
+        def bucketName = 'test_bucket_' + (new Random().nextInt(10**4))
+        def bucket = client.createBucket(bucketName)
 
-    // write
-    def dirPath = "${homePath}/test-data/tmp/"
-    object.writeTo(dirPath)
-    def file = Paths.get(dirPath + object.name)
-    assert Files.exists(file)
-    Files.delete(file)
+        def homePath = new File("").getAbsoluteFile().toString()
+        def fileName = 'txt1.txt'
+        bucket.putBulk("${homePath}/test-data/dir1/" + fileName)
+        def object = bucket.object(fileName)
+        def meta = object.getMetadata()
+        assert meta['name']
+        assert meta['size'] == object.size
+        assert 10 < object.size
+        assert meta['owner']
+        assert meta['bucketName']
 
-    // delete
-    object.delete()
-    shouldFail { bucket.object(fileName) }
-    bucket.delete()
-  }
+        /* write */
+        def dirPath = "${homePath}/test-data/tmp/"
+        object.writeTo(dirPath)
+        def file = Paths.get(dirPath + object.name)
+        assert Files.exists(file)
+        Files.delete(file)
+
+        /* delete */
+        object.delete()
+        shouldFail { bucket.object(fileName) }
+        bucket.delete()
+    }
 
 }
