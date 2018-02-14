9eaf7d19857a25a29615450855a46a8f6a640a19
Sage Thomas <sage.thomas@outlook.com>
2018-02-13 22:58:45 -0700

    updated bucket test

--- src/test/groovy/com/spectralogic/dsl/test/models/BpBucketTest.groovy
+++ src/test/groovy/com/spectralogic/dsl/test/models/BpBucketTest.groovy
@@ -1,74 +1,121 @@
 package com.spectralogic.dsl.test.models
 
-import com.spectralogic.dsl.exceptions.BpException
 import com.spectralogic.dsl.models.BpClientBuilder
 import org.apache.commons.io.FileUtils
 import org.junit.Test
 
-import java.nio.file.Files
-import java.nio.file.Paths
-
-/** Tests BpBucket */
 class BpBucketTest extends GroovyTestCase {
-
-  @Test
-  void testBucket() throws IOException {
     def homePath = new File("").getAbsoluteFile().toString()
     def tmpPath = "$homePath/test-data/tmp/"
-    FileUtils.cleanDirectory(new File(tmpPath))
 
-    def client = new BpClientBuilder().create()
-    def bucketName = 'test_bucket_' + (new Random().nextInt(10 ** 4))
-    def bucket = client.createBucket(bucketName)
+    def createContext() {
+        FileUtils.cleanDirectory(new File(tmpPath))
 
-    /* test put */
-    bucket.putBulk("${homePath}/test-data/dir2")
-    bucket.putBulk(["${homePath}/test-data/dir1/txt1.txt",
-                    "${homePath}/test-data/dir1/txt2.txt",
-                    "${homePath}/test-data/dir1/txt3.txt"])
-    bucket.putBulk("$homePath/test-data/dir3/txt6.txt", "remote/")
-    bucket.putBulk("$homePath/test-data/dir4/", "remote2/")
-    def fileCount = 8
+        def client = new BpClientBuilder().create()
+        def bucketName = 'test_bucket_' + (new Random().nextInt(10**4))
 
-    /* test bucket metadata */
-    assertEquals fileCount, bucket.objects().size()
-    assertEquals bucket.objects().size(), bucket.reload().objects().size()
-    assertEquals 'txt1.txt', bucket.object('txt1.txt').name
-    assertEquals 2, bucket.objects('txt2.txt', 'txt3.txt').size()
-    assert 10000 < bucket.object('txt2.txt').size
-
-    /* test get specific objects */
-    def objNames = ['txt1.txt','txt2.txt','txt3.txt']
-    shouldFail { bucket.getBulk(['doesnt_exist.txt'], tmpPath) }
-    bucket.getBulk(objNames, tmpPath)
-    objNames.each {
-      def file = Paths.get(tmpPath)
-      assert Files.exists(file)
+        return [client.createBucket(bucketName), bucketName, client]
     }
 
-    /* test get objects in remote directory */
-    bucket.getBulk("remote/txt6.txt", tmpPath)
-    def file = new File("${tmpPath}remote/txt6.txt")
-    assert file.exists()
-
-    /* test get object of remote directory */
-    bucket.getBulk("remote2/", tmpPath)
-    [new File("${tmpPath}remote2/txt7.txt"), new File("${tmpPath}remote2/txt8.txt")].each {
-      assert it.exists()
+    @Test
+    void testDeleteBucket() {
+        def (bucket, bucketName, client) = createContext()
+        bucket.delete()
+        shouldFail { client.bucket(bucketName) }
     }
 
-    FileUtils.cleanDirectory(new File(tmpPath))
+    @Test
+    void testDeleteObjects() {
+        def (bucket, bucketName, client) = createContext()
 
-    /* test object deletion */
-    shouldFail BpException.class, { bucket.delete() }
-    bucket.deleteObjects([bucket.object('txt1.txt')])
-    assertEquals fileCount - 1, bucket.objects().size()
-    bucket.deleteAllObjects()
-    assertEquals 0, bucket.objects().size()
+        try {
+            assertEquals 0, bucket.size()
+            bucket.putBulk(["${homePath}/test-data/dir1/txt1.txt",
+                            "${homePath}/test-data/dir1/txt2.txt",
+                            "${homePath}/test-data/dir1/txt3.txt",
+                            "${homePath}/test-data/dir1/txt4.txt"])
+            assertEquals 4, bucket.size()
 
-    /* test bucket deletion */
-    bucket.delete()
-    shouldFail { client.bucket(bucketName) }
-  }
+            bucket.deleteObject(bucket.object('txt1.txt'))
+            assertEquals 3, bucket.size()
+
+            bucket.deleteObjects([bucket.object('txt2.txt'),
+                                  bucket.object('txt3.txt')])
+            assertEquals 1, bucket.size()
+
+            bucket.deleteAllObjects()
+            assertEquals 0, bucket.size()
+        } finally {
+            bucket.deleteAllObjects()
+            bucket.delete()
+        }
+    }
+
+    @Test
+    void testPutFile() {
+        def (bucket, bucketName, client) = createContext()
+
+        try {
+            assertEquals 0, bucket.size()
+            bucket.putBulk("$homePath/test-data/dir2/txt5.txt")
+            assertEquals 1, bucket.size()
+
+            bucket.putBulk(["${homePath}/test-data/dir1/txt1.txt",
+                            "${homePath}/test-data/dir1/txt2.txt",
+                            "${homePath}/test-data/dir1/txt3.txt"])
+            assertEquals 4, bucket.size()
+        } finally {
+            bucket.deleteAllObjects()
+            bucket.delete()
+        }
+    }
+
+    @Test
+    void testPutDirectory() {
+        def (bucket, bucketName, client) = createContext()
+
+        try {
+            assertEquals 0, bucket.size()
+            bucket.putBulk("$homePath/test-data/dir3/")
+            assertEquals 2, bucket.size()
+        } finally {
+            bucket.deleteAllObjects()
+            bucket.delete()
+        }
+    }
+
+    @Test
+    void testGetObject() {
+        def (bucket, bucketName, client) = createContext()
+
+        try {
+            bucket.putBulk("$homePath/test-data/dir3/", "remote/")
+            bucket.getBulk(["remote/txt6.txt", "remote/subdir/txt7.txt"], tmpPath)
+            def file = new File("${tmpPath}remote/txt6.txt")
+            assert file.exists()
+            file = new File("${tmpPath}remote/subdir/txt7.txt")
+            assert file.exists()
+        } finally {
+            bucket.deleteAllObjects()
+            bucket.delete()
+        }
+    }
+
+    @Test
+    void testGetDirectory() {
+        def (bucket, bucketName, client) = createContext()
+
+        try {
+            bucket.putBulk("$homePath/test-data/dir3/", "remote/")
+            bucket.getBulk(["remote/txt6.txt", "remote/subdir/txt7.txt"], tmpPath)
+            def file = new File("${tmpPath}remote/txt6.txt")
+            assert file.exists()
+            file = new File("${tmpPath}remote/subdir/txt7.txt")
+            assert file.exists()
+        } finally {
+            bucket.deleteAllObjects()
+            bucket.delete()
+        }
+    }
 
 }
